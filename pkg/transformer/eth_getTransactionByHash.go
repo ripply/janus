0fe655fcf2b39eda824b67fb589368244ad14052
dezuman <johannes.borg@hotmail.com>
2022-01-26 14:06:48 +0100

    Restore call to current non-working function to get a reference test run

--- pkg/transformer/eth_getTransactionByHash.go
+++ pkg/transformer/eth_getTransactionByHash.go
@@ -162,12 +162,14 @@ func getTransactionByHash(p *qtum.Qtum, hash string) (*eth.GetTransactionByHashR
 		// TODO: Figure out proper way to do this
 		// There is a problem with this function, sometimes it returns errors on regtest, empty block?
 		// commenting it out as its being overwritten below anyway
-		/*
-			ethTx.From, err = getNonContractTxSenderAddress(p, qtumDecodedRawTx.Vins)
-			if err != nil {
-				return nil, eth.NewCallbackError("couldn't get non contract transaction sender address")
-			}
-		*/
+		
+		// TODO 2 - Electric bugaloo: Attempt to get this working properly, wish me luck
+		
+		ethTx.From, err = getNonContractTxSenderAddress(p, qtumDecodedRawTx.Vins)
+		if err != nil {
+			return nil, eth.NewCallbackError("couldn't get non contract transaction sender address")
+		}
+		
 		// TODO: discuss
 		// ? Does func above return incorrect address for graph-node (len is < 40)
 		// ! Temporary solution
